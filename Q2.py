e0c4d1c1df43439ad23c25746ac6c7d997d20aa0
Shankster2791 <85214391+Shankster2791@users.noreply.github.com>
2021-06-09 12:42:16 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,4 +1,6 @@
+#Q2. Find cubic root of 27, 64, 891 using sciPy special package.
+
 from scipy.special import cbrt
 
-res = cbrt([27, 64, 891])
+res = cbrt([27, 64, 891]) #Element-wise cube root of array.
 print(res)
