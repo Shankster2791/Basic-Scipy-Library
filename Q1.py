5ad8aa73d1bf0bf7400e174af7fa7572379845a6
Shankster2791 <85214391+Shankster2791@users.noreply.github.com>
2021-06-09 12:40:49 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,3 +1,6 @@
+#Q1. Import the essential library scipy with i/o package and Numpy. Create a 4 x 4 dimensional
+#one's array. Store array in test.text file. Get data from test.text file and print the output.
+
 import scipy.io as sio
 import numpy as np
 
@@ -6,6 +9,6 @@ x = np.array([[1, 2, 3,4],
               [9,10,11,12],
               [13,14,15,16]])
 
-sio.savemat('test.mat', {"f":x})
-data = sio.loadmat('test.mat')
+sio.savemat('test.txt', {"f":x})
+data = sio.loadmat('test.txt')
 print(data)
