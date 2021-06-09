4fa9f578875f39ec62b8317ad576a0a75292d45d
Shankster2791 <85214391+Shankster2791@users.noreply.github.com>
2021-06-09 12:42:50 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,3 +1,7 @@
+#Q4. Define two-dimensional array with values {(5,4),(6,3)}. Output eigen values and
+#eigenvectors of the matrix.
+
+
 import scipy.linalg as la
 import numpy as np
 
@@ -5,7 +9,7 @@ import numpy as np
 x = np.array([[5,4], 
               [6,3]])
 
-eigvals, eigvecs = la.eig(x)
-print(eigvals)
+eigvals, eigvecs = la.eig(x) #eigen function for a square matrix. Return eigen values and eigen vector.
+print(eigvals) #eigen values
 print("\n")
-print(eigvecs)
+print(eigvecs) #eigen vector
