7e8143a9e85217e1f7029ca3358b6ab5cd7fe764
Shankster2791 <85214391+Shankster2791@users.noreply.github.com>
2021-06-09 12:42:32 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,11 +1,16 @@
+#Q3. Create two matrices with 2x2 dimensions. Initialize them with values [4,5], [3,2]. Calculate
+#determinant of a two-dimensional matrix using scipy.linalg. Calculate the inverse of a matrix
+#in 3.
+
 from scipy import linalg
 import numpy as np
 
 x = np.array([[4,5], 
               [3,2]])
-y = linalg.det(x)
 
-z = linalg.inv(x)
+y = linalg.det(x) #Compute the determinant of a matrix
+
+z = linalg.inv(x) #Compute the inverse of a matrix.
 
 print(y)
 
